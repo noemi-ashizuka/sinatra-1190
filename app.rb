562fb843630dba57fa8c9dff683e37435c44ef12
Noemi Ashizuka <noemi.calanducci@gmail.com>
2023-04-24 18:00:13 +0900

    Livecode

--- app.rb
+++ app.rb
@@ -2,7 +2,32 @@ require_relative "config/application"
 require "sinatra"
 require "sinatra/reloader"
 require "sinatra/activerecord"
+require "pry-byebug"
+
+# router + controller
 
 get "/" do
-  "Hello world!"
+  # "Hello batch 1190!"
+  @restaurants = Restaurant.all
+  erb :index
 end
+
+# /restaurants/1
+get "/restaurants/:id" do
+  id = params[:id]
+  @restaurant = Restaurant.find(id)
+  erb :show
+end
+
+post "/restaurants" do
+  name = params[:name]
+  city = params[:city]
+  restaurant = Restaurant.new(name: name, city: city)
+  restaurant.save
+  redirect "/"
+end
+
+# GET to get data 
+# POST to create/add data
+# PUT/PATCH update
+# DELETE 
